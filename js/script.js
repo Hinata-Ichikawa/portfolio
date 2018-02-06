3005c530330f79b118d862c57932eeac2f858861
市川ひなた <hinakami0701@gmail.com>
2018-02-07 02:09:55 +0900

    途中経過のところまでを最初のコミットとした

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(function(){
+	var mySwiper = new Swiper ('.swiper-container', {
+		loop: false,
+		pagination: '.swiper-pagination',
+		// nextButton: '.swiper-button-next',
+		// prevButton: '.swiper-button-prev',
+		speed :450,
+
+		// navigation: {
+  //           nextEl: '.swiper-button-next',
+  //           prevEl: '.swiper-button-prev',
+  //       },
+
+        keyboardControl :true,
+	})
+});
